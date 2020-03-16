9657e5116eddbc184e6096e2a6a55bf9461d9f5b
dherrada <dylan.herrada@gmail.com>
2020-03-16 15:04:50 -0400

    Ran black, updated to pylint 2.x

--- examples/crickit_drive_simpletest.py
+++ examples/crickit_drive_simpletest.py
@@ -7,7 +7,7 @@ from adafruit_crickit import crickit
 # For Circuit Playground Express, micro:bit, and RPi Crickit, use:
 drive_1 = crickit.drive_1
 # For Feather Crickit, use:
-#drive_1 = crickit.feather_drive_1
+# drive_1 = crickit.feather_drive_1
 
 # Turn on Drive 1 for 1 second and then off for 1 second
 while True:
